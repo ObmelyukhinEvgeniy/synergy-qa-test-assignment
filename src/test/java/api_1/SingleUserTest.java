b2a3287a0bf23af48a2df569e8ebd88519ec19ea
ObmelyukhinEvgeniy <polozhitelnyy88@mail.ru>
2024-01-08 11:59:40 +0300

    Next 1

--- src/test/java/api_1/SingleUserTest.java
+++ src/test/java/api_1/SingleUserTest.java
@@ -1,20 +1,63 @@
 package api_1;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.util.List;
-import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class Test {
-    private final static String URL = "https://reqres.in";
-    @org.junit.jupiter.api.Test
-    public void checkCode200Test() {
-        List<PojoClassBaseData> user = given()
-                .when()
-                .contentType(ContentType.JSON)
-                .get(URL + "/api/users/2")
-                .then().log().all()
-                .extract().body().jsonPath().getList("", PojoClassBaseData.class);
-                //.statusCode(200);
-                int i = 0;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SingleUserTest {
+    @Test(description = "Check 200 OK", priority = 0)
+    public void getUserSingleOK200Test() {
+        Specifications.installSpecification(Specifications.requestSpecification("https://reqres.in"), Specifications.responseSpecificationOK200());
+            given()
+                    .spec(Specifications.requestSpecification("https://reqres.in"))
+            .when()
+                    .get("/api/users/2")
+            .then()
+                    .spec(Specifications.responseSpecificationOK200())
+                    .assertThat()
+                    .body("data.id", equalTo(2));
+
+//        Assert.assertEquals(response.getStatusCode(), 200);
+//        Assert.assertEquals(response.jsonPath().getInt("data.id"), 2);
     }
+    @Test(description = "Check 404 Not Found", priority = 1)
+    public void getUserSingleError404Test() {
+        //Specifications.installSpecification(Specifications.requestSpecification("https://reqres.in"), Specifications.responseSpecificationError404());
+        given()
+                .spec(Specifications.requestSpecification("https://reqres.in"))
+        .when()
+                .get("/api/users/9999")
+        .then()
+                .spec(Specifications.responseSpecificationError404());
+    }
+
+    /*
+    @Test
+    public void checkCode200Test() {
+        PojoClassBaseData user = RestAssured
+                .given()
+                    .baseUri("https://reqres.in")
+                    .log().all()
+                .when()
+                    .contentType(ContentType.JSON)
+                    .get("/api/users/2")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .extract().response().as(PojoClassBaseData.class);
+    }
+    */
+
 }
--- src/test/java/api_1/Specifications.java
+++ src/test/java/api_1/Specifications.java
@@ -1,2 +1,40 @@
-package api_1;public class Specifications {
+package api_1;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpecification(String baseURL) {
+        return new RequestSpecBuilder()
+                .setBaseUri("https://reqres.in")
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecificationOK200() {
+        return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecificationError404() {
+        return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .expectStatusCode(404)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static void installSpecification(RequestSpecification request, ResponseSpecification response) {
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
 }
